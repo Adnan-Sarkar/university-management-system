70620565de81dafd417cd038386ea703b5a2a7ee
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-19 03:58:50 +0600

    Add middleware into store and allow credentials in basiApi for getting cookie from server.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
+import baseApi from "./api/baseApi";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
