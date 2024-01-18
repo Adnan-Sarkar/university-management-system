90bd3c1a4308e3c73ebe3949380a54125892da57
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-19 04:12:31 +0600

    Implement redux-persist into store and provide PersistGate into main route

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import baseApi from "./api/baseApi";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddlewares) =>
@@ -17,3 +26,5 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
+
+export const persistor = persistStore(store);
