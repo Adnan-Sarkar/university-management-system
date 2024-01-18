b12974bc7472ae5bc7e7e76e3e13bf40fc189c05
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-18 23:45:15 +0600

    Reuse types from type folder and remove irrelevant types

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,16 +1,7 @@
-import { ReactNode } from "react";
 import AdminDemo1 from "../pages/admin/AdminDemo1";
 import AdminDemo2 from "../pages/admin/AdminDemo2";
 import AdminDemo3 from "../pages/admin/AdminDemo3";
 import AdminDemo4 from "../pages/admin/AdminDemo4";
-import { NavLink } from "react-router-dom";
-
-type TSidebarItem = {
-  key: string;
-  label: ReactNode;
-  children?: TSidebarItem[];
-};
-
 // demo routes
 export const adminPaths = [
   {
@@ -39,31 +30,3 @@ export const adminPaths = [
     ],
   },
 ];
-
-// create ant design sidebar menu items
-export const adminSidebarItems = adminPaths.reduce(
-  (acc: TSidebarItem[], item) => {
-    if (item.path && item.name) {
-      acc.push({
-        key: item.name,
-        label: <NavLink to={`/admin/${item.path}`}>{item.path}</NavLink>,
-      });
-    } else if (item.children) {
-      acc.push({
-        key: item.name,
-        label: item.name,
-        children: item.children.map((element) => {
-          return {
-            key: element.name,
-            label: (
-              <NavLink to={`/admin/${element.path}`}>{element.path}</NavLink>
-            ),
-          };
-        }),
-      });
-    }
-
-    return acc;
-  },
-  []
-);
