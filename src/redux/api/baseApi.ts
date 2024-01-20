8e9dd30a101e5d4848c3fb04246d8acc92bc5c1f
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-21 02:15:37 +0600

    Implement a wrapper for fetchBaseQuery to set new access token by using refresh token if current access token is expired.

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,14 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  DefinitionType,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { logOut, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -15,9 +24,51 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  // calling baseQuery to fetch api endpoints
+  let result = await baseQuery(args, api, extraOptions);
+
+  // if access token isexpired, then sending refresh token to retrive new access token
+  if (result?.error?.status === 401) {
+    const res = await fetch(
+      `http://localhost:5000/api/v1//auth/refresh-token`,
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
+
+    const data = await res.json();
+
+    const user = (api.getState() as RootState).auth.user;
+    const token = data.data.accessToken;
+
+    if (!token) {
+      api.dispatch(logOut());
+    }
+
+    api.dispatch(
+      setUser({
+        user,
+        token,
+      })
+    );
+
+    // if baseQuery not fetched for expired access token,
+    // then again fetch baseQuery after setting acccess token
+    result = await baseQuery(args, api, extraOptions);
+  }
+
+  return result;
+};
+
 const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
 
