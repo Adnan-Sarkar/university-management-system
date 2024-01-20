503e13954495eb30049c52d4c4d0d88b58d76e0d
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-20 23:39:15 +0600

    Add toast message using sonner package

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,27 +4,45 @@ import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
 import { setUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
+  const navigate = useNavigate();
   const { register, handleSubmit } = useForm();
   const [login, { isError }] = useLoginMutation();
   const dispatch = useAppDispatch();
 
   const onSubmit = async (data) => {
-    const userInfo = {
-      id: data.id,
-      password: data.password,
-    };
-    const res = await login(userInfo).unwrap();
+    const toastId = toast.loading("Logging In");
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
 
-    const user = verifyToken(res.data.accessToken);
+      const user = verifyToken(res.data.accessToken);
 
-    dispatch(
-      setUser({
-        user: user,
-        token: res.data.accessToken,
-      })
-    );
+      dispatch(
+        setUser({
+          user: user,
+          token: res.data.accessToken,
+        })
+      );
+
+      toast.success("Logged In", {
+        id: toastId,
+        duration: 1000,
+      });
+
+      navigate(`/${user.role}/dashboard`);
+    } catch (err) {
+      toast.error("Something went wrong!", {
+        id: toastId,
+        duration: 1000,
+      });
+    }
   };
 
   return (
