a8e8b14d9adc579d46d6b698ffceabb025c31960
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-21 00:01:01 +0600

    Implement dynamic sidebat items based on user role

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,60 @@
+import { Layout, Menu, MenuProps } from "antd";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { adminPaths } from "../../routes/admin.routes";
+import { facultyPaths } from "../../routes/faculty.routes";
+import { studentPaths } from "../../routes/student.routes";
+import { useAppSelector } from "../../redux/hooks";
+import { TUser } from "../../redux/features/auth/authSlice";
+const { Sider } = Layout;
+
+const userRole = {
+  ADMIN: "admin",
+  FACULTY: "faculty",
+  STUDENT: "student",
+};
+
+const Sidebar = () => {
+  const { role } = useAppSelector((state) => state.auth.user) as TUser;
+
+  let sidebarItems;
+
+  switch (role) {
+    case userRole.ADMIN:
+      sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
+      break;
+    case userRole.FACULTY:
+      sidebarItems = sidebarItemsGenerator(facultyPaths, userRole.FACULTY);
+      break;
+    case userRole.STUDENT:
+      sidebarItems = sidebarItemsGenerator(studentPaths, userRole.STUDENT);
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <Sider breakpoint="lg" collapsedWidth="0">
+      <div
+        className="demo-logo-vertical"
+        style={{
+          color: "#fff",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "64px",
+          borderBottom: "1px solid #999",
+        }}
+      >
+        <h1>LOGO</h1>
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        items={sidebarItems as MenuProps["items"]}
+      />
+    </Sider>
+  );
+};
+
+export default Sidebar;
