40088ea21615ba5a1b21c83ca671a1956915f007
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-21 03:05:31 +0600

    Add academic semester route into admin routes

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -2,12 +2,23 @@ import AdminDemo1 from "../pages/admin/AdminDemo1";
 import AdminDemo2 from "../pages/admin/AdminDemo2";
 import AdminDemo3 from "../pages/admin/AdminDemo3";
 import AdminDemo4 from "../pages/admin/AdminDemo4";
+import AcademicSemester from "../pages/admin/academicManagement/AcademicSemester";
 // demo routes
 export const adminPaths = [
   {
     name: "Dashboard",
     path: "dashboard",
     element: <AdminDemo1 />,
+  },
+  {
+    name: "Academic Management",
+    children: [
+      {
+        name: "Academic Semesters",
+        path: "academic-semesters",
+        element: <AcademicSemester />,
+      },
+    ],
   },
   {
     name: "User Management",
