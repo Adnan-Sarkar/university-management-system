c51b9ffd85f46d5d0bedb3366aabc22d20ac9f6e
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-02-06 23:26:54 +0600

    Set new routes for admin to handle academic management.

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -2,7 +2,12 @@ import AdminDemo1 from "../pages/admin/AdminDemo1";
 import AdminDemo2 from "../pages/admin/AdminDemo2";
 import AdminDemo3 from "../pages/admin/AdminDemo3";
 import AdminDemo4 from "../pages/admin/AdminDemo4";
+import AcademicDepartment from "../pages/admin/academicManagement/AcademicDepartment";
+import AcademicFaculty from "../pages/admin/academicManagement/AcademicFaculty";
 import AcademicSemester from "../pages/admin/academicManagement/AcademicSemester";
+import CreateAcademicDepartment from "../pages/admin/academicManagement/CreateAcademicDepartment";
+import CreateAcademicFaculty from "../pages/admin/academicManagement/CreateAcademicFaculty";
+import CreateAcademicSemester from "../pages/admin/academicManagement/CreateAcademicSemester";
 // demo routes
 export const adminPaths = [
   {
@@ -14,9 +19,34 @@ export const adminPaths = [
     name: "Academic Management",
     children: [
       {
+        name: "Create Academic Semesters",
+        path: "create-academic-semesters",
+        element: <CreateAcademicSemester />,
+      },
+      {
         name: "Academic Semesters",
         path: "academic-semesters",
         element: <AcademicSemester />,
+      },
+      {
+        name: "Create Academic Faculty",
+        path: "create-academic-faculty",
+        element: <CreateAcademicFaculty />,
+      },
+      {
+        name: "Academic Faculty",
+        path: "academic-faculty",
+        element: <AcademicFaculty />,
+      },
+      {
+        name: "Create Academic Department",
+        path: "create-academic-department",
+        element: <CreateAcademicDepartment />,
+      },
+      {
+        name: "Academic Department",
+        path: "academic-department",
+        element: <AcademicDepartment />,
       },
     ],
   },
